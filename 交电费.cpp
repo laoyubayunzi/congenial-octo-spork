75c67186487b479a9d4b8641ff33c3220de72e34
laoyubayunzi <qwertyuiopwqs123456@outlook.com>
2025-10-18 18:59:15 +0800

    Add files via upload

---
+++ 交电费.cpp
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+#include<stdlib.h>
+int main()
+{
+	double n=0;
+	double r=0;
+	scanf("%lf",&n);//分类讨论
+	if(n<=150&&n>0)
+	{
+	r=n*0.44630;
+	printf("%.11lf",r);
+	}
+	else if(n>150&&n<=400)
+	{
+	r=150*0.44630+(n-150)*0.46630;
+	printf("%.11lf",r);
+	}
+	else if(n>400)
+	{
+	r=150*0.44630+250*0.46630+(n-400)*0.56630;
+	printf("%.11lf",r);
+	}
+
+    system("pause");
+    return 0;
+}
