bde185966a7f4e72f6a41e3782c10265e1123d71
laoyubayunzi <qwertyuiopwqs123456@outlook.com>
2025-10-18 10:11:54 +0800

    Add files via upload

---
+++ 99乘法表for.cpp
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+int main() {
+   
+	for(int i=1;i<=9;i++)
+	{
+	    for(int j=1;j<=9;j++)
+	     {
+			 if(j>i) {break;}
+	       printf("%d×%d=%2d ",j,i,i*j);
+	
+		 }printf("\n");
+	}
+
+
+
+	system("pause");
+    return 0;
+}
---
+++ 99乘法表while.cpp
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+int main() {
+	int i=1;
+	int j=1;
+
+   
+	while(i <= 9) 
+	{
+		j=1;
+		
+	    while(j <= 9) 
+	      {
+			  if(j>i) {break;}
+	       printf("%d×%d=%2d ",j,i,i*j);
+             j++;
+          }
+     i++;
+	 printf("\n");
+    }
+
+
+
+	system("pause");
+    return 0;
+}
